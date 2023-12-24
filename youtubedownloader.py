47774ade4c8452b006b4f5185093fe542c18f7ae
Beosti <ziroxismail@gmail.com>
2023-12-24 02:19:57 +0100

    Made it do so you can also download videos and streamlining the code

--- youtubedownloader.py
+++ youtubedownloader.py
@@ -1,4 +1,5 @@
-from pytube import YouTube
+from pytube import YouTube, Playlist
+from pytube.exceptions import VideoUnavailable
 import os
 
 
@@ -19,19 +20,43 @@ def create_video_folder(folder_name):
         print(f"Folder '{folder_name}' inside '{video_path}' already exists.")
 
 
-videoLink = input("Link: ")
-videoFormat = input("Video or Audio: ")
-folderName = input("Folder name: ")
-create_video_folder(folderName)
-youtube = YouTube(videoLink)
-if videoFormat == "Video":
-    video = youtube.streams.get_highest_resolution()  # gets ready to download
-    video.download('/home/shibeo/Videos/' + folderName)
-    print("Download Complete of video")
-elif videoFormat == "Audio":
-    video = youtube.streams.filter(only_audio=True).first()
-    video.download('/home/shibeo/Videos/' + folderName, filename=f"{video.title}.mp3")
-    print("Download Complete of audio")
-else:
-    print("Not a valid format!")
-    exit()
+def download_video(video_link, video_format, folder_name):
+    create_video_folder(folder_name)
+    try:
+        if "playlist" in video_link.lower():
+            playlist = Playlist(video_link)
+            for video in playlist.videos:
+                download_single_video(video, video_format, folder_name)
+        else:
+            youtube = YouTube(video_link)
+            download_single_video(youtube, video_format, folder_name)
+    except VideoUnavailable as e:
+        print(f"Error: {e}")
+        print("The video is unavailable or age-restricted. Skipping download.")
+    except Exception as e:
+        print(f"Error: {e}")
+        print("An unexpected error occurred. Skipping download.")
+
+
+def download_single_video(video, video_format, folder_name):
+    try:
+        if video_format == "Video":
+            stream = video.streams.get_highest_resolution()  # gets ready to download
+            stream.download(f'/home/shibeo/Videos/{folder_name}')
+            print(f"Download Complete of video: {video.title}")
+        elif video_format == "Audio":
+            stream = video.streams.filter(only_audio=True).first()
+            stream.download(f'/home/shibeo/Videos/{folder_name}', filename=f"{video.title}.mp3")
+            print(f"Download Complete of audio: {video.title}")
+        else:
+            print("Not a valid format!")
+    except Exception as e:
+        print(f"Error: {e}")
+        print(f"An unexpected error occurred while downloading {video.title}. Skipping download.")
+
+
+if __name__ == "__main__":
+    videoLink = input("Link: ")
+    videoFormat = input("Video or Audio: ")
+    folderName = input("Folder name: ")
+    download_video(videoLink, videoFormat, folderName)
